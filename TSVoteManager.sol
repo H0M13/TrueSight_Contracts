ca55f149a0eb45dea9885aa4b624218b0fd4960c
MATTOP\Matthew <m.durkin92@gmail.com>
2021-05-19 16:04:53 +0100

    Make vote manager contract ownable. Allow owner to set cid of images directory

--- TSVoteManager.sol
+++ TSVoteManager.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/Ownable.sol";
 
-contract TSVoteManager {
-    
-    bytes32[] imageCids;
+contract TSVoteManager is Ownable {
     
     /////////////////
     // Domain objects
@@ -37,7 +36,7 @@ contract TSVoteManager {
     }
     
     mapping(bytes32 => LabelsVote) public keyTolabelsVoteStruct;
-    mapping(bytes32 => uint) public cidLabelsVoteCount; 
+    mapping(bytes32 => uint) public cidLabelsVotesCount; 
     bytes32[] public labelsVoteList;
     
     struct LabelsVoteInput {
@@ -48,6 +47,9 @@ contract TSVoteManager {
         uint8 disturbing;
         uint8 hate;
     }
+    
+    // The content ID of the images directory
+    bytes32 public imagesDirCid;
     
     /////////////////
     // Events
@@ -77,19 +79,19 @@ contract TSVoteManager {
         return keyTolabelsVoteStruct[_key].key == _key;
     }
     
-    function getVoterLabelsVoteCount(address _voterAddress) external view returns(uint labelsVoteCount) {
+    function getVoterLabelsVoteCount (address _voterAddress) external view returns(uint labelsVoteCount) {
         require (isVoter(_voterAddress), "No voter exists for this address");
         return addresstoVoterStruct[_voterAddress].labelsVoteKeys.length;
     }
     
-    function getVotersLabelsVoteAtIndex(address _voterAddress, uint _row) external view returns(bytes32 key) {
+    function getVotersLabelsVoteAtIndex (address _voterAddress, uint _row) external view returns(bytes32 key) {
         require (isVoter(_voterAddress), "No voter exists for this address");
         return addresstoVoterStruct[_voterAddress].labelsVoteKeys[_row];
     }
 
-    function getLabelsVoteKeys(bytes32 _cid) external view returns(bytes32[] memory keys) {
-        require (cidLabelsVoteCount[_cid] > 0, "No votes for this cid");
-        bytes32[] memory labelsVoteKeys = new bytes32[](cidLabelsVoteCount[_cid]);
+    function getLabelsVoteKeys (bytes32 _cid) external view returns(bytes32[] memory keys) {
+        require (cidLabelsVotesCount[_cid] > 0, "No votes for this cid");
+        bytes32[] memory labelsVoteKeys = new bytes32[](cidLabelsVotesCount[_cid]);
         uint counter = 0;
         for (uint i = 0; i < labelsVoteList.length; i++) {
             if (keyTolabelsVoteStruct[labelsVoteList[i]].cid == _cid) {
@@ -104,7 +106,12 @@ contract TSVoteManager {
     // State changing functions
     /////////////////
     
-    function createVoter() public returns(bool success) {
+    function setImagesDirCid(bytes32 _cid) external onlyOwner returns(bool success) {
+        imagesDirCid = _cid;
+        return true;
+    }
+    
+    function createVoter() external returns(bool success) {
         require(!isVoter(msg.sender), "Voter already exists");
         addresstoVoterStruct[msg.sender].voterAddress = msg.sender;
         voterList.push(msg.sender);
@@ -112,7 +119,7 @@ contract TSVoteManager {
         return true;
     }
     
-    function createLabelsVotes(LabelsVoteInput[] memory _labelsVoteInputs) public returns(bool success) {
+    function createLabelsVotes(LabelsVoteInput[] memory _labelsVoteInputs) external returns(bool success) {
         require (isVoter(msg.sender), "No voter exists for this address");
         for (uint i = 0; i < _labelsVoteInputs.length; i++) {
             bytes32 key = keccak256(abi.encodePacked(msg.sender, _labelsVoteInputs[i].cid));
@@ -129,7 +136,7 @@ contract TSVoteManager {
                 });
             if (!isExistingVote) {
                 labelsVoteList.push(key);
-                cidLabelsVoteCount[_labelsVoteInputs[i].cid]++;
+                cidLabelsVotesCount[_labelsVoteInputs[i].cid]++;
                 addresstoVoterStruct[msg.sender].labelsVoteKeys.push(key);
                 emit NewLabelsVote(msg.sender, key);
             }
